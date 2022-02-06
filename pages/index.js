7f8ee573cd7c46b888b21fb0866f7ecc30205c8e
Tuag <igortuag@gmail.com>
2022-02-06 08:59:16 -0300

    add styles to list of champions

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home({ listOfChampionsJson }) {
               className={styles.card}
             >
               <h2>{champion.name}</h2>
-              <p>{champion.title}</p>
               <Image
                 width={100}
                 height={100}
